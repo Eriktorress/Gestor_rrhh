d4f7e1337e56d084f530048c73469dcbabca505c
Erik <101225590+Eriktorress@users.noreply.github.com>
2023-05-18 09:29:18 -0400

    Revert "Base datos"

--- apps/Personal/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PersonalConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.Personal'
--- apps/Personal/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Personal
-
-class PersonalSerializer(serializers.ModelSerializer):
-    class Meta:
-        model= Personal
-        fields = '__all__'
