d2fc509dc3b667f40be5fcfe0a4c6dd8a4032b41
Borisivc <101225512+Borisivc@users.noreply.github.com>
2023-05-22 18:20:08 -0400

    cambios
    creacion de tablas

--- apps/Personal/models.py
+++ apps/Personal/models.py
@@ -2,9 +2,17 @@ from django.db import models
 import email 
 
 class Personal(models.Model):
+<<<<<<< Updated upstream
     nombre = models.CharField (verbose_name='Nombres_per', max_length=20, null=False)
     apellido = models.CharField (verbose_name='Apellidos_per', max_length=40, null=False)
     rut = models.CharField (verbose_name='Rut', max_length=9, null=False)
     dv = models.CharField (verbose_name='Digito_verificador', max_length=1, null=False)
     email = models.EmailField ()
 
+=======
+    nombre = models.CharField (verbose_name='nombres_per', max_length=20, null=False)
+    apellido = models.CharField (verbose_name='apellidos_per', max_length=40, null=False)
+    rut = models.CharField (verbose_name='rut', max_length=9, null=False)
+    dv = models.CharField (verbose_name='digito_verificador', max_length=1, null=False)
+    email = models.EmailField ()
+>>>>>>> Stashed changes
