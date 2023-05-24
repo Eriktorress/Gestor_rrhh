ff93d7afa754be2adef5d1df988d69fb220c03b9
Borisivc <101225512+Borisivc@users.noreply.github.com>
2023-05-24 18:45:36 -0400

    cambios

--- src/containers/pages/Trabajadores.jsx
+++ src/containers/pages/Trabajadores.jsx
@@ -1,13 +1,53 @@
 import Layout from "hocs/layouts/Layout"
+import Table from 'react-bootstrap/Table';
+/*import index from 'src/styles/'*/
 
 
 function Trabajadores() {
-    return(
+    return (
         <Layout>
-            Trabajadores
+            <Table striped>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombres_per</th>
+                        <th>Apellidos_per</th>
+                        <th>Rut</th>
+                        <th>DV</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody /*class="Centrar"*/>
+                    <tr>
+                        <td>1</td>
+                        <td>Boris</td>
+                        <td>Villarroel</td>
+                        <td>16790973</td>
+                        <td >6</td>
+                        <td>brsivc@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Erik</td>
+                        <td>Torres</td>
+                        <td>15876924</td>
+                        <td>2</td>
+                        <td>don_erik@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Ignacio</td>
+                        <td>Dinamarca</td>
+                        <td>21349852</td>
+                        <td>8</td>
+                        <td>don_nacho@gmail.com</td>
+                    </tr>
+                </tbody>
+            </Table>
         </Layout>
+
     )
-    
+
 }
 
 export default Trabajadores
